b20415afcc56b047d4e07e92f7010cba26adf11c
Rodrigo Mello <rodbove@gmail.com>
2023-07-09 12:51:30 -0300

    feat: add initial functions concept for stock management

--- src/curso/core.clj
+++ src/curso/core.clj
@@ -1,3 +1,22 @@
 (ns curso.core)
 
-(println "Bem vindo ao sistema de estoque")
+(println "Stock management system")
+
+(defn apply-discount? [brute-value]
+  (when (> brute-value 100)
+    true))
+
+(println (apply-discount? 1000))
+(println (apply-discount? 90))
+
+(defn discounted-value
+  "Returns the brute value with a 10% discount if it is higher than 100"
+  [brute-value]
+  (if (apply-discount? brute-value)
+    (let [discount-tax (/ 10 100)
+          discount (* brute-value discount-tax)]
+      (- brute-value discount))
+    brute-value))
+
+(println (discounted-value 1000))
+(println (discounted-value 100))
